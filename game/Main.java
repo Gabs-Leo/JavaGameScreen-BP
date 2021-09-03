b2a4b173e7236acd5e2c3e608da65f07f7d08c46
Gabs-Leo <gabrieldemoraistaveira@gmail.com>
2021-09-03 19:07:05 -0300

    Some Comments
    Adding some comments for code explanation.

--- game/Main.java
+++ game/Main.java
@@ -1,8 +1,11 @@
 package com.gabs.game;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -12,19 +15,27 @@ import javax.swing.JFrame;
  * @author gabs
  */
 public class Main extends Canvas implements Runnable{
-    //public static int WIDTH = 160, HEIGHT = 90, SCALE = ;
+    //Screen Size
+    public static int WIDTH = 1600, HEIGHT = 900;
     
-    Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-    public int WIDTH = (int)size.getWidth(), HEIGHT = (int)size.getHeight();
+    //TODO Comment the line above and uncomment the 2 lines below to get responsive full screen
+    /*Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+    public int WIDTH = (int)size.getWidth(), HEIGHT = (int)size.getHeight();*/
+    
+    //Screen FPS and SCALE
+    public static int SCALE = 1;
+    public static int FPSLIMIT = 60;
     
     public Main(){
-        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
     }
     
     public static void main(String[] args) {
+        //Main class object for Rhread and JFrame
         Main main = new Main();
-        
         JFrame frame = new JFrame();
+        
+        //JFrame Configs
         frame.add(main);
         frame.setTitle("MyGame!");
         frame.pack();
@@ -32,29 +43,52 @@ public class Main extends Canvas implements Runnable{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         
-        System.out.println("oi!");
-        
+        //Starting new Thread
         new Thread(main).start();
-        
     }
 
+    //Game Looping for logic
     public void eventTick(){
         
     }
+    //Game render
     public void render(){
         
+        //Buffer strategy for graphic otimization
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        
+        //Drawing Graphics
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.black);
+        //Black Background [X, Y, Xsize, YYsize]
+        g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+        
+        //Magenta Dot
+        g.setColor(Color.magenta);
+        g.fillRect(WIDTH*SCALE/2 - 20/2, HEIGHT*SCALE/2 - 20/2, 20, 20);
+        
+        //Show on Screen
+        bs.show();
     }
     
-    
+    //Game Thread
     @Override
     public void run() {
+        //Looping for tick and render
         while(true){
             eventTick();
             render();
+            
             try {
-                Thread.sleep(1000/60);
+                //Limit fps for 60 for try and catch
+                Thread.sleep(1000/FPSLIMIT);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("FPS LIMIT Error!");
             }
         }
     }
